f54395e08488b8e414c4677f5bcb31bbf7b0b5d3
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-12-01 16:33:26 +0300

    Sort map APIs

--- libraries/stdlib/src/kotlin/collections/JUtil.kt
+++ libraries/stdlib/src/kotlin/collections/JUtil.kt
@@ -21,8 +21,6 @@ private object EmptyList : List<Any> {
     override fun toString(): String = list.toString()
 }
 
-private object EmptyMap : Map<Any, Any> by HashMap<Any, Any>() {}
-
 private object EmptySet : Set<Any> {
     private val set = HashSet<Any>()
 
@@ -37,7 +35,6 @@ private object EmptySet : Set<Any> {
 }
 
 public fun emptyList<T>(): List<T> = EmptyList as List<T>
-public fun emptyMap<K, V>(): Map<K, V> = EmptyMap as Map<K, V>
 public fun emptySet<T>(): Set<T> = EmptySet as Set<T>
 
 /** Returns a new read-only list of given elements */
@@ -45,12 +42,6 @@ public fun listOf<T>(vararg values: T): List<T> = if (values.size() == 0) emptyL
 
 /** Returns an empty list */
 public fun listOf<T>(): List<T> = emptyList()
-
-/** Returns a new read-only map of given pairs, where the first value is the key, and the second is value */
-public fun mapOf<K, V>(vararg values: Pair<K, V>): Map<K, V> = if (values.size() == 0) emptyMap() else linkedMapOf(*values)
-
-/** Returns an empty read-only map */
-public fun mapOf<K, V>(): Map<K, V> = emptyMap()
 
 /** Returns a new read-only set of given elements */
 public fun setOf<T>(vararg values: T): Set<T> = if (values.size() == 0) emptySet() else values.toCollection(LinkedHashSet<T>())
@@ -64,30 +55,6 @@ public fun arrayListOf<T>(vararg values: T): ArrayList<T> = values.toCollection(
 /** Returns a new HashSet with a variable number of initial elements */
 public fun hashSetOf<T>(vararg values: T): HashSet<T> = values.toCollection(HashSet(values.size()))
 
-/**
- * Returns a new [[HashMap]] populated with the given pairs where the first value in each pair
- * is the key and the second value is the value
- *
- * @includeFunctionBody ../../test/collections/MapTest.kt createUsingPairs
- */
-public fun <K, V> hashMapOf(vararg values: Pair<K, V>): HashMap<K, V> {
-    val answer = HashMap<K, V>(values.size())
-    answer.putAll(*values)
-    return answer
-}
-
-/**
- * Returns a new [[LinkedHashMap]] populated with the given pairs where the first value in each pair
- * is the key and the second value is the value. This map preserves insertion order so iterating through
- * the map's entries will be in the same order
- *
- * @includeFunctionBody ../../test/collections/MapTest.kt createLinkedMap
- */
-public fun <K, V> linkedMapOf(vararg values: Pair<K, V>): LinkedHashMap<K, V> {
-    val answer = LinkedHashMap<K, V>(values.size())
-    answer.putAll(*values)
-    return answer
-}
 
 public val Collection<*>.indices: IntRange
     get() = 0..size() - 1
@@ -103,7 +70,6 @@ public val Int.indices: IntRange
 public val <T> List<T>.lastIndex: Int
     get() = this.size() - 1
 
-
 /** Returns true if the collection is not empty */
 public fun <T> Collection<T>.isNotEmpty(): Boolean = !this.isEmpty()
 
@@ -113,8 +79,6 @@ public val Collection<*>.notEmpty: Boolean
 
 /** Returns the Collection if its not null otherwise it returns the empty list */
 public fun <T> Collection<T>?.orEmpty(): Collection<T> = this ?: emptyList()
-
-// List APIs
 
 /** Returns the List if its not null otherwise returns the empty list */
 public fun <T> List<T>?.orEmpty(): List<T> = this ?: emptyList()
