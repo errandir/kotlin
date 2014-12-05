eedfea37348b5f942cfab684f120632c4679666d
Valentin Kipyatkov <valentin@jetbrains.com>
2014-12-05 08:44:29 +0300

    Correct completion of nested/inner classes after dot
     #KT-1889 Fixed

---
+++ idea/testData/completion/smart/InnerClass.kt
@@ -0,0 +1,16 @@
+trait T
+
+class C {
+    inner class Inner1(s: String) : T
+    inner class Inner2 : T
+    inner class Inner3
+}
+
+fun foo(c: C): T {
+    return c.<caret>
+}
+
+// EXIST: { lookupString: "Inner1", itemText: "Inner1", tailText: "(s: String)", typeText: "C.Inner1" }
+// EXIST: { lookupString: "Inner2", itemText: "Inner2", tailText: "()", typeText: "C.Inner2" }
+// ABSENT: Inner3
+// NUMBER: 2
